57bf549b4f44be79a62d1e72d59130a5d1b7d742
Andrés Machetá <sr.macheta08@gmail.com>
2025-09-09 15:09:11 -0500

    feat: Install Express and set up initial server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// 1. Importar Express
+const express = require('express');
+
+// 2. Crear una instancia de la aplicación Express
+const app = express();
+
+// 3. Definir el puerto en el que correrá el servidor
+const PORT = 5000; 
+
+// 4. Crear nuestro primer endpoint de prueba
+// Cuando alguien haga una petición GET a la raíz '/', se ejecutará esta función
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo! El servidor de Huizy está funcionando.');
+});
+
+// 5. Poner el servidor a "escuchar" en el puerto definido
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
+});
