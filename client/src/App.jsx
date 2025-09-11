9d8de2365dc1ee26a96c7b71057ddd17922122c0
Andrés Machetá <sr.macheta08@gmail.com>
2025-09-11 15:17:41 -0500

    feat(client): Set up and configure Tailwind CSS v4

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+// client/src/App.jsx
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-extrabold text-teal-600 underline">
+        Página de Huizy
+      </h1>
+    </div>
+  )
+}
+export default App
---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+// client/vite.config.js
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+})
