1f8cbf948b9c4bd023efef58c11a7031727c53ef
Andrés Machetá <sr.macheta08@gmail.com>
2025-10-16 16:08:46 -0500

    fix(admin): Import Link to create product and edit product pages

--- client/src/pages/ProductEditPages.jsx
+++ client/src/pages/ProductEditPages.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 
 function ProductEditPage() {
